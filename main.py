ed4affa7d294a8c93bbf867bfc212504c962b98b
igparish <133685768+igparish@users.noreply.github.com>
2023-06-12 18:28:01 +0000

    Made more changes

--- main.py
+++ main.py
@@ -1,27 +1,90 @@
-import pickle
-from os import path
+from sklearn.metrics import SCORERS
+import serialize
 
-def save (object, file):
-    """Serialize a dictionary to a file."""
-    with open(file, "wb") as f:
-        pickle.dump(object, f)
+def play_trivia(questions):
 
-def load(file):
-    """Load a dictionary from a serialized file.
-     Or return None if the file doesn't exist."""
-    if path.exists(file):
-        with open(file, "rb") as f:
-            return pickle.load(f)
-    return None
+    score = 0
 
-FILE = "state.pkl"
-d = load (FILE)
-if d is None:
-    d = {}
+    total_questions = len(questions)
 
-print (d)
 
-key = input ("enter a key to add to the dictionary")
-value = input ("enter a value to add to the dictionary")
-d[key] = value
-save (d, FILE)
+    for question in questions:
+
+        print(question['question'])
+
+        for i, option in enumerate(question['options']):
+
+            print(f"{i + 1}. {option}")
+
+
+        answer = input("Enter your answer (1-4): ")
+
+        if answer == str(question['correct_answer']):
+
+            print("Correct!")
+
+            score += 1
+
+        else:
+
+            print("Incorrect!")
+
+
+    print("\nTrivia Complete!")
+
+    print(f"Your score: {score}/{total_questions}")
+    return score
+
+
+def main():
+
+    # Example trivia questions
+
+    questions = [
+
+        {
+
+            'question': 'What is the capital of France?',
+
+            'options': ['London', 'Paris', 'Berlin', 'Madrid'],
+
+            'correct_answer': 2
+
+        },
+
+        {
+
+            'question': 'Who painted the Mona Lisa?',
+
+            'options': ['Leonardo da Vinci', 'Pablo Picasso', 'Vincent van Gogh', 'Michelangelo'],
+
+            'correct_answer': 1
+
+        },
+
+        {
+
+            'question': 'What is the largest planet in our solar system?',
+
+            'options': ['Mars', 'Venus', 'Jupiter', 'Saturn'],
+
+            'correct_answer': 3
+
+        }
+
+    ]
+
+
+    # Play trivia game
+
+    file = "last_score.pkl"
+    last_score = serialize.load(file)
+    if last_score is not None:
+        print ("Welcome back, you last score was", last_score)
+
+    last_score = play_trivia(questions)
+    serialize.save(last_score, file)
+
+if __name__ == '__main__':
+
+    main() 
