a7c134b38f04ff78174610f846496a42a2e2968a
igparish <133685768+igparish@users.noreply.github.com>
2023-06-12 18:27:09 +0000

    Made changes

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("hello")
+import pickle
+from os import path
+
+def save (object, file):
+    """Serialize a dictionary to a file."""
+    with open(file, "wb") as f:
+        pickle.dump(object, f)
+
+def load(file):
+    """Load a dictionary from a serialized file.
+     Or return None if the file doesn't exist."""
+    if path.exists(file):
+        with open(file, "rb") as f:
+            return pickle.load(f)
+    return None
+
+FILE = "state.pkl"
+d = load (FILE)
+if d is None:
+    d = {}
+
+print (d)
+
+key = input ("enter a key to add to the dictionary")
+value = input ("enter a value to add to the dictionary")
+d[key] = value
+save (d, FILE)
